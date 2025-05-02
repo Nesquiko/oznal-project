9dc77ced90ad0750b162c343c0d98ad7314d39cc
Michal-Kilian <kilian.michal.kili@gmail.com>
2025-05-02 19:01:43 +0200

    navigation, upload page, eda page

--- shiny/shiny-app.R
+++ shiny/shiny-app.R
@@ -6,14 +6,14 @@ predict_winner <- function(minute, t1_kills, t2_kills, first_blood, first_tower,
   return("Team 1")
 }
 
-ui <- fluidPage(
-  useShinyjs(),
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
-  ),
-  
-  conditionalPanel(
-    condition = "output.show_input_panel",
+predictor_ui <- tabPanel(
+  "Predictor",
+  fluidPage(
+    useShinyjs(),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
+    
     fluidRow(
       column(5, div(class="header-box", "GAME CONCLUSION PREDICTOR")),
       column(2, div(class="header-logo", tags$img(src="lol-logo-.png", width = "100px", style="display: flex; ", alt="Logo"))),
@@ -103,42 +103,116 @@ ui <- fluidPage(
                  actionButton("predict_button", "PREDICT")
              )
       ),
-    )
-  ),
-  
-  conditionalPanel(
-    condition = "output.show_output_panel",
+    ),
     fluidRow(
       column(8, offset = 2, align = "center",
-             uiOutput("prediction_display"),
-             br(),
-             actionButton("reset_button", "New Prediction")
+             uiOutput("prediction_display")
       )
     )
-  ),
+  )
+)
+
+
+data_upload_ui <- tabPanel(
+  "Data Upload",
+  fluidRow(
+    column(8, offset = 2,
+           div(style = "background-color: rgba(0, 50, 78, 0.9); padding: 20px; border-radius: 15px; border: 1px solid var(--lol-gold);",
+               fileInput("file", "Choose CSV File",
+                         multiple = FALSE,
+                         accept = c("text/csv",
+                                    "text/comma-separated-values,text/plain",
+                                    ".csv")),
+               verbatimTextOutput("summary")
+           )
+    )
+  )
+)
+
+eda_ui <- tabPanel(
+  "Exploratory Data Analysis",
+  fluidRow(
+    column(8, offset = 2,
+           div(style = "background-color: rgba(0, 50, 78, 0.9); padding: 20px; border-radius: 15px; border: 1px solid var(--lol-gold);",
+               plotOutput("plot")
+           )
+    )
+  )
+)
+
+ui <- tags$div(
+  style = "width: 100%; padding: 0; margin: 0;",
+  navbarPage(
+    title = "Game Conclusion Predictor",
+    id = "navbar",
+    predictor_ui,
+    data_upload_ui,
+    eda_ui,
+    header = tags$style(HTML("
+      body {
+        padding: 0;
+        margin: 0;
+      }
+      #navbar {
+        margin: 0;
+      }
+      .navbar {
+        margin-bottom: 0;
+        border-radius: 0;
+        background-color: var(--lol-dark-blue);
+        border: none;
+      }
+      .navbar-default .navbar-nav > li > a {
+        color: var(--lol-gold);
+      }
+      .navbar-default .navbar-brand {
+        color: var(--lol-gold);
+      }
+      .tab-content {
+        padding-top: 20px;
+      }
+    ")),
+    windowTitle = "Game Conclusion Predictor",
+    collapsible = TRUE,
+    theme = "default",
+    inverse = TRUE
+  )
 )
 
 server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    req(input$file)
+    summary(read.csv(input$file$datapath))
+  })
+
   prediction_result <- reactiveVal(NULL)
   view_state <- reactiveVal("input")
   
-  output$show_input_panel <- reactive({
-    view_state() == "input"
+  output$prediction_display <- renderUI({
+    req(prediction_result())
+    predicted_winner <- prediction_result()
+    
+    tags$div(
+      p("Prediction:", class="prediction-headline"),
+      h2(paste(predicted_winner, "is predicted to win."), class="prediction-text"),
+      h4(ifelse(predicted_winner == "Team 1", 
+                "If you're playing as Team 1, you should keep playing!", 
+                "If you're playing as Team 2, you should keep playing!")),
+      h4(ifelse(predicted_winner == "Team 1", 
+                "If you're playing as Team 2, you should forfeit!",
+                "If you're playing as Team 1, you should forfeit!")),
+      br(),
+      actionButton("reset_button", "Reset")
+    )
   })
-  outputOptions(output, "show_input_panel", suspendWhenHidden = FALSE)
-  
-  output$show_output_panel <- reactive({
-    view_state() == "output"
-  })
-  outputOptions(output, "show_output_panel", suspendWhenHidden = FALSE)
   
   observeEvent(input$predict_button, {
     pred <- predict_winner(
       minute = input$minute,
       t1_kills = input$t1_kills,
       t2_kills = input$t2_kills,
-      first_blood = input$first_blood,
-      first_tower = input$first_tower,
+      first_blood = input$firstKill,
+      first_tower = input$firstTower,
       t1_towers = input$t1_towers,
       t2_towers = input$t2_towers
     )
@@ -151,20 +225,8 @@ server <- function(input, output, session) {
     view_state("input")
   })
   
-  output$prediction_display <- renderUI({
-    req(prediction_result())
-    predicted_winner <- prediction_result()
-    
-    tags$div(
-      p("Prediction:", class="prediction-headline"),
-      h2(paste(predicted_winner, "is predicted to win."), class="prediction-text"),
-      h4(ifelse(predicted_winner == "Team 1", 
-                 "If you're playing as Team 1, you should keep playing!", 
-                 "If you're playing as Team 2, you should keep playing!")),
-      h4(ifelse(predicted_winner == "Team 1", 
-                 "If you're playing as Team 2, you should forfeit!",
-                 "If you're playing as Team 1, you should forfeit!")),
-    )
+  output$plot <- renderPlot({
+    hist(rnorm(100))
   })
 }
 
